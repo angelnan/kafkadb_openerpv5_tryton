51d33daa89c291e231de4cbeb5b3f02d4ba07816
Angel Alvarez <angel@nan-tic.com>
2013-09-28 18:27:45 +0200

    refactor ir_property_upload

--- ir/ir_property.py
+++ ir/ir_property.py
@@ -30,60 +30,39 @@
 #
 ##############################################################################
 
-import os,sys
-
+import os
+import sys
 
 sys.path.append(os.path.abspath(os.getcwd()))
-
 from KafkaDB import tools
-
 
 config = tools.read_kettle_properties()
 source_db = tools.get_source_connection(config)
 target_db = tools.get_target_connection(config)
-
 source_cursor = source_db.cursor()
 target_cursor = target_db.cursor()
-
 migration_filename = config.get('migration_config')
 migration = tools.readConfigFile(migration_filename)
-
-ir_path=os.path.join(config.get('transformation_path'),'ir/')
+ir_path = os.path.join(config.get('transformation_path'), 'ir/')
 model_mapping = tools.readConfigFile(ir_path + 'ir_model_mapping.cfg')
-field_mapping = tools.readConfigFile(ir_path + 'ir_field_mapping.cfg')
-property_mapping = tools.readConfigFile(ir_path +'ir_property_mapping.cfg')
+#field_mapping = tools.readConfigFile(ir_path + 'ir_field_mapping.cfg')
+property_mapping = tools.readConfigFile(ir_path + 'ir_property_mapping.cfg')
+models = {}
+fields = {}
 
-model = {}
-properties = {}
-fields ={}
-
-for target,source in model_mapping['models'].iteritems():
+for target, source in model_mapping['models'].iteritems():
     if source:
-        model[source]=target
+        models[source] = target
 
 
-for target,source in property_mapping['property'].iteritems():
-    if source:
-        if not properties.get(source):
-            properties[source] =[]
-        properties[source].append(target)
-
-for m,fields_dict in field_mapping.iteritems():
-    fields[m]={}
-    for target_field,source_field in fields_dict.iteritems():
-        if not fields[m].get(source_field):
-            fields[m][source_field] = []
-        fields[m][source_field].append(target_field)
-
-def get_mapping_id(table,value_id):
+def get_mapping_id(table, value_id):
     target_cursor.execute("SELECT target FROM migration." + table +
-            " WHERE source="+str(value_id))
-    print target_cursor.query
-    target_id =  target_cursor.fetchone()
+            " WHERE source=" + str(value_id))
+    target_id = target_cursor.fetchone()
     if target_id:
         return target_id[0]
-
     return None
+
 
 def get_field_id(model, field):
     target_cursor.execute(
@@ -93,15 +72,10 @@ def get_field_id(model, field):
             "ir_model m "
             "WHERE "
             " f.model = m.id AND "
-            " m.model=%s AND"
-            " f.name in %s",(model, tuple(field)))
-    fields= target_cursor.fetchall()
-    return fields
-
-def get_target_field(model, field):
-    return fields.get(model) and \
-            fields[model].get(field)
-
+            " m.model='%s' AND"
+            " f.name = '%s'" % (model, field))
+    field, = target_cursor.fetchone()
+    return field
 
 
 def add_property(res_model, res_id, value_model, value_id, field, company):
@@ -115,80 +89,93 @@ def add_property(res_model, res_id, value_model, value_id, field, company):
     val_map = get_map_table(value_model)
 
     res_map_table = migration.get(res_map) and \
-            migration[res_map].get('mapping')
+        migration[res_map].get('mapping')
     val_map_table = migration.get(val_map) and \
-            migration[val_map].get('mapping')
+        migration[val_map].get('mapping')
 
-    if res_map_table:
-        rid=get_mapping_id(res_map_table,res_id)
+    if res_map_table and (res_map_table != 'None'):
+        rid = get_mapping_id(res_map_table, res_id)
     if val_map_table:
-        vid= get_mapping_id(val_map_table,value_id)
+        vid = get_mapping_id(val_map_table, value_id)
 
     if res_model:
-        res = res_model+","+str(rid)
+        res = res_model + "," + str(rid)
     if vid:
-        value = value_model+","+str(vid)
+        value = value_model + "," + str(vid)
 
-    tfields = get_target_field(res_model, field)
-    if not tfields:
-        print "FIELD %s not mapping yet"%tfields
-        return
+    target_cursor.execute(
+        'INSERT INTO ir_property(res, value, field, company)'
+        ' VALUES(%s,%s,%s,%s)', (res, value, field, company))
 
-    fields = get_field_id(res_model,tfields)
-    for field in fields:
-        target_cursor.execute(
-                'INSERT INTO ir_property(res,value,field,company)'
-                ' VALUES(%s,%s,%s,%s)',(res, value, field, company))
 
 def get_map_table(model):
     if model:
-        return model.replace('.','_')
+        return model.replace('.', '_')
     return model
 
 
-
-source_cursor.execute("SELECT res_id,name,value,company_id FROM ir_property")
-for row in source_cursor.fetchall():
-#    print row
-    res,name,value,company = row
-
-    if not properties.get(name):
-        print "Property %s Not Mapped yet"%name
-        continue
-
-    res_model = False
-    res_id = None
-    value_model = False
-    value_id = None
-
-    if res:
-        res_model,res_id = res.split(',')
-    if value:
-        value_model,value_id = value.split(',')
+def create_property(target_model, source_model, property, sfield, rel_model):
+    table_sm = get_map_table(source_model)
+    query = "select id, %s from %s" % (sfield, table_sm)
+    source_cursor.execute(query)
+    company = 1
+    for row in source_cursor.fetchall():
+        res_id, value_id = row
+        if value_id is None:
+            continue
+        field = get_field_id(target_model, property)
+        add_property(target_model, res_id, rel_model, value_id, field, company)
 
 
-    res_map = get_map_table(res_model)
-    val_map = get_map_table(value_model)
-    target_res = get_map_table(model.get(res_model))
-    target_val = get_map_table(model.get(value_model))
-    target_model_res = model.get(res_model)
-    target_model_val = model.get(value_model)
+def migrate_property(target_model, source_model, target, source):
+    query = ("select ip.res_id, ip.name, ip.value, ip.company_id from "
+        "ir_property as ip, "
+        "ir_model_fields as imf, "
+        "ir_model as im  "
+        "where imf.model_id = im.id and "
+        "   ip.fields_id = imf.id and "
+        "   ip.name='%s' "
+        "   and ip.value is not null and "
+        " im.model = '%s'" % (source, source_model))
+
+    source_cursor.execute(query)
+    for row in source_cursor.fetchall():
+        res, name, value, company = row
+        res_model = False
+        res_id = None
+        value_model = False
+        value_id = None
+
+        if res:
+            res_model, res_id = res.split(',')
+        if value:
+            value_model, value_id = value.split(',')
+
+        target_model_res = models.get(res_model)
+        target_model_val = models.get(value_model)
+        field = get_field_id(target_model, target)
+
+        add_property(target_model_res, res_id, target_model_val,
+            value_id, field, company)
 
 
-    if not model.get(value_model) or not model.get(res_model) or \
-           not migration.get(target_res) or \
-           not migration.get(target_val):
-        print "Models not mapped yet, check %s,%s"%(value_model,res_model)
-        continue
+if __name__ == '__main__':
 
-    add_property(target_model_res,res_id, target_model_val,value_id, name, company)
+    for section in property_mapping:
+        module, target_model = section.split('/')
+        pm = property_mapping[section]
+        model_source = pm.pop('source')
+        convert = eval(pm.pop('convert'))
+        prop = {}
+        for ptarget, psource in pm.iteritems():
+            tp, model = tuple(psource.split(','))
 
+            if tp in convert:
+                create_property(target_model, model_source,
+                    ptarget, tp, model)
+            else:
+                migrate_property(target_model, model_source, ptarget, tp)
 
-target_db.commit()
-target_db.close()
-source_db.close()
-
-
-
-
-
+    target_db.commit()
+    target_db.close()
+    source_db.close()
